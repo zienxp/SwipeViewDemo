d42d5a8f884e0d4040124dbc826c675952691b16
ahmed.zien <ahmed.zien@gmail.com>
2020-02-08 07:02:37 +0200

    Add Menu items for the Navigation Drawer

--- app/src/main/java/com/example/swipeviewdemo/MainActivity.java
+++ app/src/main/java/com/example/swipeviewdemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.swipeviewdemo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private DemoFragmentCollectionAdpter adapter;
     private Toolbar toolbar;
     private TabLayout tabLayout;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,48 @@ public class MainActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.pager);
         adapter = new DemoFragmentCollectionAdpter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
         tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_camera:
+                        menuItem.setChecked(true);
+                        displayMessage("Import Selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+
+                    case R.id.nav_gallery:
+                        menuItem.setChecked(true);
+                        displayMessage("Gallery Selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+
+                    case R.id.nav_slideshow:
+                        menuItem.setChecked(true);
+                        displayMessage("Slide Show Selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.nav_Tools:
+                        menuItem.setChecked(true);
+                        displayMessage("Tools Selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void displayMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
