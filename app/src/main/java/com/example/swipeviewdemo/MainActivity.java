5243a90e95a85deebc1908e2f9a7d98d66e8a8cc
ahmed.zien <ahmed.zien@gmail.com>
2020-02-09 06:33:11 +0200

    Add the Navigation Drawer button to the app bar

--- app/src/main/java/com/example/swipeviewdemo/MainActivity.java
+++ app/src/main/java/com/example/swipeviewdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+
         viewPager = findViewById(R.id.pager);
         adapter = new DemoFragmentCollectionAdpter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
@@ -35,13 +38,21 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         navigationView = findViewById(R.id.navigationView);
+
+
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.nav_camera:
                         menuItem.setChecked(true);
                         displayMessage("Import Selected...");
@@ -71,7 +82,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void displayMessage(String message){
+    private void displayMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
